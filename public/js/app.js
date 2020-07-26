97122abee097c43488e1b8d470fcb719d8eb0af6
sonaliagrawa <sonali12agrawal@gmail.com>
2020-07-26 13:23:56 -0700

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,3 @@
-console.log('first page')
-
-
-
-
-
-
 const weatherForm =document.querySelector('form')
 const search= document.querySelector('input')
 const messageOne= document.querySelector('#message-1')
